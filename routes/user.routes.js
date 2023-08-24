cb1edd604281899d113b4d9fc844111283af0524
jonp001 <jonperez01@outlook.com>
2023-08-23 20:20:00 -0400

    pushing branch work

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -14,8 +14,9 @@ router.get('/:userId', (req, res, next) => {
         if (!user) {
           return res.status(404).json({ error: 'User not found' });
         }
-        const { name, bio, image, level } = user;
-        res.status(200).json({ name, bio, image, level });
+        
+        const {_id, name, bio, img, level, stravaProfile } = user;
+        res.status(200).json(user);
       })
       .catch(err => next(err));
   });
@@ -25,20 +26,21 @@ router.put('/profile', isAuthenticated, (req, res, next) => {
     const userId= req.payload._id;
 
     //This is the data that can be updated
-    const { bio, image, level } = req.body;
+    const { bio, img, level, stravaProfile } = req.body;
 
     //This create new object with the properties that are able to be updated
     const updates = {
         ...(bio && { bio}),
-        ...(image && { image}),
-        ...(level && { level })
+        ...(img && { img}),
+        ...(level && { level }),
+        ...(stravaProfile && { stravaProfile })
     };
 
     User.findByIdAndUpdate(userId, updates, { new: true})
     .then(updatedUser => {
        //return updated user data
-       const { bio, image, level} = updatedUser;
-       res.status(200).json({ bio, image, level });
+       const { bio, img, level, stravaProfile} = updatedUser;
+       res.status(200).json({ bio, img, level, stravaProfile });
   })
   .catch(err => next(err));
 });
